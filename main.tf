cb8143c31cfb8836145c7266ac20dfce0119ad59
viknesh742 <54580316+viknesh742@users.noreply.github.com>
2021-10-19 19:46:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,18 +11,14 @@ resource "azurerm_resource_group" "RG" {
   }
 }
 module "network" {
-  source              = "Azure/network/VNET"
-  resource_group_name = azurerm_resource_group.RG.name
-  address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24""]
-  subnet_names        = ["subnet1", "subnet2"]
-
-  subnet_service_endpoints = {
-    "subnet1" : ["VYDNDSN1"], 
-    "subnet2" : ["VYDNDSN2"]
-  }
-
-  tags = {
+  source               = "Azure/network"
+  vNetRg               = azurerm_resource_group.RG.name
+  vNetAddressSpace     = ["10.0.0.0/16", "10.2.0.0/16"]
+  subnet1Address       = ["10.0.1.0/24"]
+  subnet1              = ["subnet1"]
+  subnet1Address       = ["10.0.2.0/24"]
+  subnet1              = ["subnet2"]
+  vNetTags = {
     Name = "VYDND"
     environment = "Test"
   }
