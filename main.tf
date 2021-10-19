3dd87f3862c0988a4339caae21a8cc2b608e595a
viknesh742 <54580316+viknesh742@users.noreply.github.com>
2021-10-19 19:20:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "RG" {
   }
 }
 module "network" {
-  source              = "Azure/network/azurerm"
+  source              = "Azure/network/VNET"
   resource_group_name = azurerm_resource_group.example.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24""]
