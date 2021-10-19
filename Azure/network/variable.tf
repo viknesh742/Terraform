77c230db266e8ed3bf497b68e20ad3b5d2d51dc7
viknesh742 <54580316+viknesh742@users.noreply.github.com>
2021-10-19 19:37:00 +0530

    Create variable.tf

---
+++ Azure/network/variable.tf
@@ -0,0 +1,44 @@
+variable "vNetName" {
+  type        = string
+  description = "Name of the Vnet"
+}
+
+variable "vNetLocation" {
+  type        = string
+  description = "location of the Vnet"
+}
+
+variable "vNetRg" {
+  type        = string
+  description = "Vnet Resource Group"
+}
+
+variable "vNetAddressSpace" {
+  type        = string
+  description = "Address space of Vnet"
+}
+
+variable "subnet1" {
+  type        = string
+  description = "Name of 1st Subnet"
+}
+
+variable "subnet2" {
+  type        = string
+  description = "Name of 2nd subnet"
+}
+
+variable "subnet1Address" {
+  type        = string
+  description = "Address of 1st Subnet"
+}
+
+variable "subnet2Address" {
+  type        = string
+  description = "Address of 2nd Subnet"
+}
+
+variable "vNetTags" {
+  type        = map(string)
+  description = "Tags for Vnet"
+}
