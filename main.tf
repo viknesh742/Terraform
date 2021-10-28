1b83238ff474682db928c6dc3fb0a4882f3a2cf4
viknesh742 <54580316+viknesh742@users.noreply.github.com>
2021-10-28 13:39:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,11 +33,9 @@ resource "azurerm_resource_group" "RG" {
 module "network" {
   source               = "./Azure/network"
   vNetRg               = azurerm_resource_group.RG.name
-  vNetAddressSpace     = ["10.0.0.0/16", "10.2.0.0/16"]
+  vNetAddressSpace     = "10.0.0.0/16"
   subnet1Address       = "10.0.1.0/24"
   subnet1              = "subnet1"
-  subnet2Address       = "10.0.2.0/24"
-  subnet2              = "subnet2"
   vNetTags = {
     Name = "VYDND"
     environment = "Test"
